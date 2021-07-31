eb210a85491d8ea7fbbb057494afa41f15802aa6
osa212 <78644067+osa212@users.noreply.github.com>
2021-07-31 21:26:32 +0900

    Initial Commit

---
+++ travelExpenses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travelExpenses
+//
+//  Created by Sargylana Osipova on 31.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
